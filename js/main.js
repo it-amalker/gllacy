16b104740e50bab93b0f5e480553d4e68968cdee
amalker <it-amalker@yandex.ru>
2019-11-02 21:59:21 +0300

    add script for popup open and close

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 let allSwitches = document.querySelectorAll(".switch");
 let allSliders = document.querySelectorAll(".slider");
 let bodyBgColors = ['#849d8f', '#8996a6', '#9d8b84'];
+
+let overlayPopup = document.querySelector(".overlay");
+let modalPopup = document.querySelector(".modal-feedback");
+let openPopupButton = document.querySelector(".contact-map-button");
+let closePopupButton = document.querySelector(".feedback-close-button");
+
 // Sliders switch
 
 let toggleSliders = function (allSwitches, switchElement, allSliders) {
@@ -25,3 +31,25 @@ let launchToggleSliders = function (switchesList, slidersList) {
 }
 
 launchToggleSliders(allSwitches, allSliders);
+
+// feedback popup
+
+if (modalPopup) {
+    openPopupButton.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        overlayPopup.classList.add("overlay--show");
+        modalPopup.classList.add("modal-feedback--show");
+    })
+
+    closePopupButton.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        overlayPopup.classList.remove("overlay--show");
+    })
+
+    overlayPopup.addEventListener("click", function () {
+        overlayPopup.classList.remove("overlay--show");
+    });
+    modalPopup.addEventListener("click", function (evt) {
+        evt.stopPropagation();
+    });
+}
